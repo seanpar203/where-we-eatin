2012f93c0704ca1d6b13d950276a8e39220566a3
Sean Parsons <sean@sean.dev>
2016-02-11 13:38:15 -0500

    Created occasions.js factory for the icons and maintaining selected occasion state

---
+++ app/assets/javascripts/factories/occasions.js
@@ -0,0 +1,15 @@
+export default class Occasions {
+  constructor() {
+    this.occasions = [
+      {name: 'breakfast'},
+      {name: 'lunch'},
+      {name: 'dinner'},
+      {name: 'drinks'},
+      {name: 'pizza'},
+      {name: 'desert'},
+      {name: 'sushi'},
+      {name: 'ice-cream'}
+    ];
+    this.selectedOccasion = {};
+  }
+}
