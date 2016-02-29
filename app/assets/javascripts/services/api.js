dac0fce8af9cd68121ffa075d76110aaedada766
Sean Parsons <sean@labelinteractive.com>
2016-02-29 09:22:06 -0500

    Updated proper URI

--- app/assets/javascripts/services/api.js
+++ app/assets/javascripts/services/api.js
@@ -10,7 +10,7 @@ class ApiService {
     this.http = axios.create();
   }
   Search(url, data) {
-    return this.http.post(url, data, {baseURL: 'http://www.where-we-eating.com:8080/'})
+    return this.http.post(url, data, {baseURL: 'http://www.where-we-eating.com/'})
   }
   getLocation(lat, lon) {
     return this.http.get(this.geo.url + lat + ',' + lon + this.geo.key)
