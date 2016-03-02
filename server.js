874d7beb3b3895514ee3d7d05a94bf567ff8325d
Sean Parsons <sean@labelinteractive.com>
2016-03-02 17:05:48 -0500

    Added a very

--- server.js
+++ server.js
@@ -13,12 +13,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-
 app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.vary('Accept-Encoding');
     next();
 });
 
