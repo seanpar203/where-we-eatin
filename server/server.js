5516b6218e4dc687fdc3a25e6c7516bacefbea7a
Sean Parsons <sean@labelinteractive.com>
2016-03-01 21:08:39 -0500

    Fixed server side code to be consistent with whats on server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 8080;
+const PORT = 80;
 const Yelp = require('./yelp');
-
 
 /**
  * Middleware Config
@@ -14,22 +13,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + '/public/'));
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+app.get('*', function(req,res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 app.post('/search', (req, res) => {
-    console.log(req.body.term);
-    Yelp.search({ term: req.body.term , location: req.body.location, limit: 15, radius_filter: 16093})
-        .then(function (data) {
-            res.json(data)
-        })
-        .catch(function (err) {
-            res.send(err.message);
-        });
+    Yelp.search({
+        term: req.body.term ,
+        location: req.body.location,
+        limit: 10,
+        radius_filter: 12874.8
+    })
+    .then(function (data) {
+        res.json(data)
+    })
+    .catch(function (err) {
+        res.send(err.message);
+    });
 });
 
 app.listen(PORT, () => {
